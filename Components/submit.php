64623cfbcde51c009ac4ce2f82758d11712c0b8e
PrzemoNo1 <37351230+PrzemoNo1@users.noreply.github.com>
2020-03-13 22:04:29 +0100

    Abstraction on  added

--- Components/submit.php
+++ Components/submit.php
@@ -1,10 +1,12 @@
 <?php
 
-    require_once "Components/Precondtions/save_preconditions.php";
-    require_once "Components/Precondtions/send_preconditions.php";
+    require_once "Form.php";
+    require_once "Preconditions/save_preconditions.php";
+    require_once "Preconditions/send_preconditions.php";
 
     $save_preconditions = new Save_preconditions();
     $send_preconditions = new Send_preconditions();
+    $form = new Form($_POST);
 
     if (isset($_POST['save']))
     {
