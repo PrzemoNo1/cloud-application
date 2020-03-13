27cedc15f9fe6932b0093cd49944354f227aaeff
PrzemoNo1 <37351230+PrzemoNo1@users.noreply.github.com>
2020-03-13 21:29:41 +0100

    save_operation creation in startup.php

--- Components/Operations/save_operation.php
+++ Components/Operations/save_operation.php
@@ -1,5 +1,16 @@
 <?php
+    @session_start();
 
-    session_start();
+    class Save_operation
+    {
+        private $_database_wrapper;
+        public function __construct($database_wrapper)
+        {
+            $_database_wrapper = $database_wrapper;
+        }
+        public function execute()
+        {
 
+        }
+    }
 ?>
--- startup.php
+++ startup.php
@@ -1,10 +1,12 @@
 <?php
-    require_once "database_wrapper.php";
+    require_once "Components/Database/database_wrapper.php";
+    require_once "save_operation.php";
     @session_start();
 
     function setUp() {
         $database_wrapper = new Database_wrapper();
+        $save_operation = new Save_operation($database_wrapper);
         $_SESSION['database_wrapper'] = $database_wrapper;
+        $_SESSION['save_operation'] = $save_operation;
     }
-
 ?>
